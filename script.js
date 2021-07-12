bd608ee7a89d23f9ea29857fb33ebdc1a4df9961
payalmangtani <87275104+payalmangtani@users.noreply.github.com>
2021-07-12 11:30:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validation(){
+    var name = document.getElementById("name").nodeValue;
+    var email = document.getElementById("email").nodeValue;
+    var message = document.getElementById("message").nodeValue;
+    var text;
+
+    alert("Message sent successfully!");
+    return true;
+
+}
